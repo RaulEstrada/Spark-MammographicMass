11d85a1760b7fc60e8b6b6c78c8d4c2da0424497
RaulEstrada <raulestradaballesteros@gmail.com>
2016-12-05 13:46:41 +0100

    Data pre-processing: Delete observations where the characteristics with NA are charactersitics whose number of NAs are lower than a limit: 10

---
+++ src/main/scala/ml/RandomForestGenerator.scala
@@ -0,0 +1,5 @@
+package main.scala.ml
+
+object RandomForestGenerator {
+
+}
--- src/main/scala/preprocessing/DataPreprocessor.scala
+++ src/main/scala/preprocessing/DataPreprocessor.scala
@@ -6,11 +6,17 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.rdd.RDD
 
 object DataPreProcessor {
+    val deletionUpperLimit = 10
+
     def preprocess(session: SparkSession, filePath: String) {
         import session.implicits._
-        val observations = session.read.option("header", "true").option("inferSchema", "true")
+        var observations = session.read.option("header", "true").option("inferSchema", "true")
             .csv(filePath).as[Observation]
         println("Read file with " + observations.count() + " observations")
+        printMissingValues(session, observations)
+        println("Removing observations with columns whose number of NA is lower than: " + deletionUpperLimit)
+        observations = handleDeletionNAs(session, observations)
+        println("After deletion, dataset with size: " + observations.count())
         printMissingValues(session, observations)
     }
 
@@ -26,5 +32,14 @@ object DataPreProcessor {
         return data.flatMap(x => x.toArray()).rdd
             .reduceByKey(_+_)
             .filter(x => x._2 > 0)
+    }
+
+    def handleDeletionNAs(session: SparkSession, data: Dataset[Observation]):
+    Dataset[Observation] = {
+        import session.implicits._
+        val targetColumns = getColumnsMissingValues(session, data)
+            .filter(x => x._2 < deletionUpperLimit)
+            .map(x => x._1).collect()
+        data.na.drop("any", targetColumns).as[Observation]
     }
 }
