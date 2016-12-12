c9669b2b8cff0b9f3e59bb2f64bafa10c1022067
RaulEstrada <raulestradaballesteros@gmail.com>
2016-12-12 12:55:51 +0100

    Refactored Ensemble code

--- src/main/scala/ml/Ensembler.scala
+++ src/main/scala/ml/Ensembler.scala
@@ -6,6 +6,7 @@ import main.scala.schema.Observation
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.linalg.DenseVector
+import org.apache.spark.rdd.RDD
 
 object Ensembler {
 
@@ -13,40 +14,54 @@ object Ensembler {
 
     def ensemble(data: Dataset[Observation], target: String, session: SparkSession) {
         import session.implicits._
+
+        val ensamble = generateEnsamble(data, target, session)
+        val Array(trainingData, testData) = data.randomSplit(Array(.7, .3))
+        val collectedPredictions = predict(testData, ensamble, session)
+        computeEnsembleError(collectedPredictions, testData)
+    }
+
+    def generateEnsamble(data: Dataset[Observation], target: String,
+    session: SparkSession): Array[PipelineModel] = {
         // Generate and train individual classifiers and print their individual test errors
         val rf = RF.generate(data, target)
         val lr = LR.generate(data, target)
         val gbt = GBT.generate(data, target)
         val mpc = MPC.generate(data, target)
+        return session.sparkContext.parallelize(Array(rf, lr, gbt, mpc)).collect()
+    }
 
-        val context = session.sparkContext
-        val ensamble = context.parallelize(Array(rf, lr, gbt, mpc)).collect()
-
-        // Obtain error of classifier
-        val Array(trainingData, testData) = data.randomSplit(Array(.7, .3))
-        context.broadcast(testData)
-        val wrong = context.longAccumulator(accumulatorName)
-        var collectedPredictions = ensamble.map(x => x.transform(testData))
+    def predict(data: Dataset[Observation], ensamble: Array[PipelineModel],
+    session: SparkSession): RDD[((Double, DenseVector), Double)] = {
+        import session.implicits._
+        var collectedPredictions = ensamble.map(x => x.transform(data))
             .reduce{ (a: DataFrame, b: DataFrame) =>
                 a.select("label", "features", "prediction").union(b.select("label", "features", "prediction"))
             }.map { row =>
                 ((row.getDouble(0), row.getAs[DenseVector](1)), Array[Double](row.getDouble(2)))
             }.rdd.groupByKey().map{x =>
                 var modeValue = mode(x._2, session)
+                (x._1, modeValue)
+            }
+        return collectedPredictions
+    }
+
+    def computeEnsembleError(predictions: RDD[((Double, DenseVector), Double)],
+        data: Dataset[Observation]) {
+        var wrongPredictions = predictions.map{x =>
                 var label = x._1._1
-                println("LABEL: " + label + "\tMODE: " + modeValue)
-                if (modeValue != label) {
+                var predicted = x._2
+                if (predicted != label) {
                     1
                 } else {
                     0
                 }
             }.reduce(_+_)
-        println("ENSAMBLE approach Test Error: " + collectedPredictions.toDouble/testData.count())
+        println("ENSAMBLE approach Test Error: " + wrongPredictions.toDouble/data.count())
     }
 
     def mode(data: Iterable[Array[Double]], session: SparkSession): Double = {
         var collected = data.map(x => x).reduce((a, b) => a ++ b)
-        println("COLLECTED: " + collected.mkString(" "))
         var occurrences = Map[Double, Int]()
         var element = 0.0
         for (element <- collected) {
